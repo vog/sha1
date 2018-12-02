a1a35bdffab8d146f1b1b17db08e96fba30e5d69
Conor Hayes <conorhay@usc.edu>
2018-12-02 13:08:34 -0800

    added gitignore and extra test

--- test_sha1_verilog.cpp
+++ test_sha1_verilog.cpp
@@ -1,4 +1,5 @@
 #include "SHA1ToVerilog.hpp"
+#include <chrono>
 #include <iostream>
 
 using namespace std;
@@ -11,12 +12,28 @@ void compare(const string &result, const string &expected)
     cout << "Expected: " << expected << "  (" << state << ")" << endl;
 }
 
+void runTest(const string& input, const string& expectedOutput)
+{
+	SHA1Top checksum;
+    cout << endl;
+    cout << "Test: " << input << endl;
+
+	auto start = chrono::high_resolution_clock::now(); 
+    std::string hash = checksum.hash(input);
+	auto stop = chrono::high_resolution_clock::now(); 
+
+	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start); 
+	  
+	// To get the value of duration use the count() 
+	// member function on the duration object 
+
+    compare(hash, expectedOutput);
+
+	cout << "The hash took " << duration.count() << " microseconds." << endl; 
+}
+
 int main(int argc, const char *argv[])
 {
-    SHA1Top checksum;
-
-    cout << endl;
-    cout << "Test:     abc" << endl;
-    std::string hash = checksum.hash("abc");
-    compare(hash, "a9993e364706816aba3e25717850c26c9cd0d89d");
+    runTest("abc", "a9993e364706816aba3e25717850c26c9cd0d89d");
+    runTest("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", "84983e441c3bd26ebaae4aa1f95129e5e54670f1");
 }
